523a60b8d592226f94ca9093d6ef3608fb3ca391
Norayr-coding <norayr.abrahamyan.09@gmail.com>
2026-02-03 22:36:04 +0400

    Fixing with gitignore

--- src/.ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,8 +0,0 @@
-numerical_features = [
-'Year', 'Adult_Mortality', 'infant_deaths', 'Alcohol', 'percentage_expenditure', 'Hepatitis_B', 'Measles', 'BMI', 
-'Polio', 'Total_expenditure', 'Diphtheria', 'GDP', 'Population', 'thinness__1-19_years', 'thinness_5-9_years', 
-'Income_composition_of_resources', 'Schooling']
-
-categorical_features = ['Country', 'Status']
-
-target = 'Life_expectancy'
